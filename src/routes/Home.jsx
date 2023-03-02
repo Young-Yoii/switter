467dac7560635c275f131bbbf58b858353e27346
Young-yoii <fhqht4545@gmail.com>
2023-03-02 17:38:16 +0900

    feat: sweet create, read

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { dbService } from './../firebase';
-import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import { collection, addDoc, serverTimestamp, getDocs, query } from 'firebase/firestore';
 
 const Home = () => {
   const [sweet, setSweet] = useState('');
+  const [sweets, setSweets] = useState([]);
+  const getSweets = async () => {
+    const dBquery = query(collection(dbService, 'sweets'));
+    const querySnapShot = await getDocs(dBquery);
+    querySnapShot.forEach((doc) => {
+      const sweetObj = {
+        ...doc.data(),
+        id: doc.id,
+      };
+      setSweets((prev) => [sweetObj, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getSweets();
+  }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
     await addDoc(collection(dbService, 'sweets'), {
@@ -22,6 +37,11 @@ const Home = () => {
         <input type="text" value={sweet} onChange={onChange} placeholder="당신의 생각은?" maxLength={120} />
         <input type="submit" value="Sweet" />
       </form>
+      {sweets.map((sweet) => (
+        <div key={sweet.id}>
+          <h4>{sweet.sweet}</h4>
+        </div>
+      ))}
     </div>
   );
 };
