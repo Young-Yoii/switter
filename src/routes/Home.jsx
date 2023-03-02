e25034aa99f4d3e1f67631a1218c125cc837ea78
Young-yoii <fhqht4545@gmail.com>
2023-03-02 17:26:20 +0900

    feat: firebase db 연결

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+import { dbService } from './../firebase';
+import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+
 const Home = () => {
-  return <span>Home</span>;
+  const [sweet, setSweet] = useState('');
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await addDoc(collection(dbService, 'sweets'), {
+      sweet,
+      createdAt: serverTimestamp(),
+    });
+    setSweet('');
+  };
+  const onChange = (event) => {
+    const value = event.target.value;
+    setSweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={sweet} onChange={onChange} placeholder="당신의 생각은?" maxLength={120} />
+        <input type="submit" value="Sweet" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
